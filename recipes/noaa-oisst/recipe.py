66038fd48abd1fc3c03f59b264c8255144b6b9b9
Ryan Abernathey <ryan.abernathey@gmail.com>
2021-02-02 13:39:22 -0500

    add missing url pattern

--- recipes/noaa-oisst/recipe.py
+++ recipes/noaa-oisst/recipe.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+input_url_pattern = (
+    "https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation"
+    "/v2.1/access/avhrr/{yyyymm}/oisst-avhrr-v02r01.{yyyymmdd}.nc"
+)
 dates = pd.date_range("1981-09-01", "2021-01-05", freq="D")
 input_urls = [
     input_url_pattern.format(
