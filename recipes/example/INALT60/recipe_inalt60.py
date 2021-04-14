307611674e2d364e4fd4c65bd6520d613b7fa2e6
uchidat <uchidat@ige-template-domOK.lthe.local>
2021-04-14 22:34:02 +0200

    combine surface ocean

--- recipes/example/INALT60/recipe_inalt60.py
+++ recipes/example/INALT60/recipe_inalt60.py
@@ -1,7 +1,7 @@
 import numpy as np
 from pangeo_forge.recipe import NetCDFtoZarrSequentialRecipe
 
-def gen_url(freq, dim, variables):
+def gen_url(freqs, dim, variables):
     months = np.array([2,3,4,8,9,10], dtype=int)
     input_url_pattern = (
             "https://data.geomar.de/downloads/20.500.12085/"
@@ -12,30 +12,23 @@ def gen_url(freq, dim, variables):
         input_url_pattern.format(freq=freq, dim=dim,
                                  var=var, month=month
                                 )
+        for freq in freqs
         for var in variables
         for month in months
                  ]
     return input_urls
 
 
-surf_ocean_4h = NetCDFtoZarrSequentialRecipe(
-                                input_urls=gen_url('4h','surface',np.array(['u','v','hts'])),
+surf_ocean = NetCDFtoZarrSequentialRecipe(
+                                input_urls=gen_url(['4h','5d'],'surface',['u','v','hts']),
                                 sequence_dim="time_counter",
                                 inputs_per_chunk=1,
                                 nitems_per_input=None,
                                 target_chunks={'time_counter': 15}
-                                            )
-
-surf_ocean_5d = NetCDFtoZarrSequentialRecipe(
-                                input_urls=gen_url('5d','surface',np.array(['u','v','hts'])),
-                                sequence_dim="time_counter",
-                                inputs_per_chunk=1,
-                                nitems_per_input=None,
-                                target_chunks={'time_counter': 15}
-                                            )
+                                         )
 
 surf_flux = NetCDFtoZarrSequentialRecipe(
-                                input_urls=gen_url('1d','surface',np.array(['flux','taux','tauy'])),
+                                input_urls=gen_url(['1d'],'surface',['flux','taux','tauy']),
                                 sequence_dim="time_counter",
                                 inputs_per_chunk=1,
                                 nitems_per_input=None,
@@ -43,7 +36,7 @@ surf_flux = NetCDFtoZarrSequentialRecipe(
                                         )
 
 int_ocean = NetCDFtoZarrSequentialRecipe(
-                                input_urls=gen_url('1d','upper1000m',np.array(['ts','u','v','w'])),
+                                input_urls=gen_url(['1d'],'upper1000m',['ts','u','v','w']),
                                 sequence_dim="time_counter",
                                 inputs_per_chunk=1,
                                 nitems_per_input=None,
@@ -59,6 +52,6 @@ grid = NetCDFtoZarrSequentialRecipe(
                                    )
 
 recipe = {
-        'surf_ocean_4h':surf_ocean_4h, 'surf_ocean_5d':surf_ocean_5d,
-        'surf_flux':surf_flux, 'int_ocean':int_ocean, 'grid':grid
+        'surf_ocean':surf_ocean, 'surf_flux':surf_flux, 
+        'int_ocean':int_ocean, 'grid':grid
          }
