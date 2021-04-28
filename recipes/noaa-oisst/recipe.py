05762b252cd3c81da1f0fe0a336a50eb19ac8264
sharkinsspatial <sharkinsgis@gmail.com>
2021-04-28 16:15:12 -0500

    Update noaa-oisst to use refactored pangeo_forge patterns.

--- recipes/noaa-oisst/recipe.py
+++ recipes/noaa-oisst/recipe.py
@@ -1,20 +1,16 @@
 import pandas as pd
-from pangeo_forge.recipe import NetCDFtoZarrSequentialRecipe
+from pangeo_forge.patterns import pattern_from_file_sequence
+from pangeo_forge.recipes import XarrayZarrRecipe
 
 input_url_pattern = (
     "https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation"
     "/v2.1/access/avhrr/{yyyymm}/oisst-avhrr-v02r01.{yyyymmdd}.nc"
 )
-dates = pd.date_range("1981-09-01", "2021-01-05", freq="D")
+dates = pd.date_range("2019-09-01", "2021-01-05", freq="D")
 input_urls = [
-    input_url_pattern.format(
-        yyyymm=day.strftime("%Y%m"), yyyymmdd=day.strftime("%Y%m%d")
-    )
+    input_url_pattern.format(yyyymm=day.strftime("%Y%m"), yyyymmdd=day.strftime("%Y%m%d"))
     for day in dates
 ]
+pattern = pattern_from_file_sequence(input_urls, "time", nitems_per_file=1)
 
-recipe = NetCDFtoZarrSequentialRecipe(
-    input_urls=input_urls,
-    sequence_dim="time",
-    inputs_per_chunk=20
-)
+recipe = XarrayZarrRecipe(pattern, inputs_per_chunk=20)
