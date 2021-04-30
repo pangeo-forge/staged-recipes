357b562aa7875ab3127441f9027fe38194114152
sharkinsspatial <sharkinsgis@gmail.com>
2021-04-30 10:34:42 -0500

    Verify presence of organization level secrets.

--- .github/actions/process_recipe/action/process_recipe.py
+++ .github/actions/process_recipe/action/process_recipe.py
@@ -5,6 +5,7 @@ from pangeo_forge_prefect.flow_manager import register_flow
 
 def main():
     secrets = json.loads(os.environ["SECRETS_CONTEXT"])
+    print(secrets)
     meta_yaml = os.environ["INPUT_PATH_TO_META_YAML"]
     bakeries_yaml = os.environ["INPUT_PATH_TO_BAKERIES_YAML"]
     register_flow(meta_yaml, bakeries_yaml, secrets)
