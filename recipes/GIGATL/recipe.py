8ec53541b385d20a5af51ef0fbe56a2e6af9b89c
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-05-20 16:08:45 -0700

    recipe.py rewrite for XarrayZarrRecipe

--- recipes/GIGATL/recipe.py
+++ recipes/GIGATL/recipe.py
@@ -1,75 +1,72 @@
 import pandas as pd
-from pangeo_forge.recipe import NetCDFtoZarrSequentialRecipe
 
-datess = pd.date_range("2008-08-01", "2008-11-01", freq="5D")
-datess = pd.concat([datess, pd.date_range("2009-02-01", "2009-05-01", freq="5D")
-                   ], ignore_index=True)
+from pangeo_forge_recipes.patterns import pattern_from_file_sequence
+from pangeo_forge_recipes.recipes import XarrayZarrRecipe
 
-def get_surf_url(dates, region, dim='surf'):
-    input_url_pattern = (
-            "ftp://eftp.ifremer.fr/SWOT/"
-            "gigatl1_1h_tides_{dim}_avg_{reg:1d}_{yymmdd}.nc"
-                        )
+
+surf_dates = pd.date_range("2008-08-01", "2008-11-01", freq="5D").to_series()
+surf_dates = pd.concat(
+    [surf_dates, pd.date_range("2009-02-01", "2009-05-01", freq="5D").to_series()],
+    ignore_index=True
+)
+
+
+def get_surf_url(region, dim='surf', dates=surf_dates,):
+    pattern = (
+        "ftp://eftp.ifremer.fr/SWOT/"
+        "gigatl1_1h_tides_{dim}_avg_{reg:1d}_{yymmdd}.nc"
+    )
     input_urls = [
-        input_url_pattern.format(
-                            dim=dim,
-                            reg=region,
-                            yymmdd=day.strftime("%Y-%m-%d")
-                                )
+        pattern.format(dim=dim, reg=region, yymmdd=day.strftime("%Y-%m-%d"))
         for day in dates
-                 ]
+    ]
     return input_urls
 
 
-datesi = pd.date_range("2008-08-01", "2008-11-01", freq="1D")
-datesi = pd.concat([datesi, pd.date_range("2009-02-01", "2009-05-01", freq="1D")
-                   ], ignore_index=True)
+int_dates = pd.date_range("2008-08-01", "2008-11-01", freq="1D").to_series()
+int_dates = pd.concat(
+    [int_dates, pd.date_range("2009-02-01", "2009-05-01", freq="1D").to_series()],
+    ignore_index=True
+)
 
-def get_int_url(dates, region):
-    input_url_pattern = (
-            "sftpgula@draco.univ-brest.fr:/GIGATL1/SWOT/"
-            "gigatl1_1h_tides_region_{reg:02d}_{yymmdd}.nc"
-                        )
+
+def get_int_url(region, dates=int_dates,):
+    pattern = (
+        "sftpgula@draco.univ-brest.fr:/GIGATL1/SWOT/"
+        "gigatl1_1h_tides_region_{reg:02d}_{yymmdd}.nc"
+    )
     input_urls = [
-            input_url_pattern.format(
-                            reg=region,
-                            yymmdd=day.strftime("%Y-%m-%d")
-                                    )
-            for day in dates
-                 ]
+        pattern.format(reg=region, yymmdd=day.strftime("%Y-%m-%d"))
+        for day in dates
+    ]
     return input_urls
 
-surf_01 = NetCDFtoZarrSequentialRecipe(
-                            input_urls=get_surf_url(datess, 1),
-                            sequence_dim="time",
-                            inputs_per_chunk=1,
-                            nitems_per_input=None,
-                            target_chunks={"time":24}
-                                      )
 
-surf_02 = NetCDFtoZarrSequentialRecipe(
-                            input_urls=get_surf_url(datess, 2),
-                            sequence_dim="time",
-                            inputs_per_chunk=1,
-                            nitems_per_input=None,
-                            target_chunks={"time":24}
-                                      )
+surf_patterns = {
+    f"surf_reg_{i:02}": pattern_from_file_sequence(get_surf_url(i), concat_dim="time",)
+    for i in range(1, 3)
+}
 
-int_01 = NetCDFtoZarrSequentialRecipe(
-                            input_urls=get_int_url(datesi, 1),
-                            sequence_dim="time"
-                            inputs_per_chunk=1
-                            ntimes_per_input=None
-                            target_chunks={"time":10}
-                                     )
+surf_recipes = {
+    list(surf_patterns)[i]: (
+        XarrayZarrRecipe(surf_patterns[list(surf_patterns)[i]], target_chunks={"time": 24},)
+    )
+    for i in range(2)
+}
 
-int_02 = NetCDFtoZarrSequentialRecipe(
-                            input_urls=get_int_url(datesi, 2),
-                            sequence_dim="time",
-                            inputs_per_chunk=1,
-                            ntimes_per_input=None,
-                            target_chunks={"time":10}
-                                     )
+int_patterns = {
+    f"int_reg_{i:02}": pattern_from_file_sequence(get_int_url(i), concat_dim="time",)
+    for i in range(1, 3)
+}
 
-recipe = {'surf-reg-01':surf_01, 'surf-reg-02':surf_02,
-          'int_reg-01' :int_01,  'int-reg-02' :int_02}
+int_recipes = {
+    list(int_patterns)[i]: (
+        XarrayZarrRecipe(int_patterns[list(int_patterns)[i]], target_chunks={"time": 10},)
+    )
+    for i in range(2)
+}
+
+recipes = {
+    **surf_recipes,
+    **int_recipes,
+}
