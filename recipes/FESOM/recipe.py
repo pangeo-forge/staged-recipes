98a55124fb7cde44e815294f969e9b79f1899b10
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-06-25 09:54:51 -0700

    medium-scale rewrite

--- recipes/FESOM/recipe.py
+++ recipes/FESOM/recipe.py
@@ -1,11 +1,10 @@
+from itertools import product
+
 from pangeo_forge_recipes.patterns import ConcatDim, FilePattern, MergeDim
 from pangeo_forge_recipes.recipes import XarrayZarrRecipe
 
-months = [f"{m:02d}" for m in (2, 3, 4, 8, 9, 10)]
-concat_months = ConcatDim("time_counter", keys=months)
 
-
-def gen_url(variable, time_counter):
+def gen_url(month, variable):
     base = (
         "https://swiftbrowser.dkrz.de/public/dkrz_035d8f6ff058403bb42f8302e6badfbc/"
         "SWOT_intercomparison/"
@@ -14,19 +13,31 @@ def gen_url(variable, time_counter):
 
 
 surf_ocean_vars = ["dflux", "ssh", "sss", "sst", "tx_sur", "ty_sur", "u_surf", "v_surf"]
-int_ocean_vars = []
+# int_ocean_vars = []
 
-pattern_dict = {
-                "surf_1h": {"vars": surf_ocean_vars},
-                "int_1d": {"vars": int_ocean_vars},
+var_dict = {
+    "surf": {"vars": surf_ocean_vars},
+    # "int": {"vars": int_ocean_vars},
 }
 
 
-def create_recipe(key, patterns=pattern_dict, url_func=gen_url, concat_dim=concat_months):
-#     url_func.__defaults__ = patterns[key]["args"]
-    merge_dim = MergeDim("variable", keys=patterns[key]["vars"])
+def create_recipe(datatype, season, vars=var_dict, url_func=gen_url):
+    """
+    """
+    merge_dim = MergeDim("variable", keys=vars[datatype]["vars"])
+
+    month_range = (2, 3, 4,) if season == "fma" else (8, 9, 10,)
+    months = [f"{m:02d}" for m in month_range]
+    concat_dim = ConcatDim("month", keys=months)
+
     pattern = FilePattern(url_func, merge_dim, concat_dim)
     return XarrayZarrRecipe(pattern, target_chunks={"time": 15})
 
 
-recipes = {list(pattern_dict)[i]: create_recipe(list(pattern_dict)[i]) for i in range(2)}
+datatypes = list(var_dict)
+seasons = ("fma", "aso")
+recipes = {
+    f"FESOM/{datatype}/{season}":
+    create_recipe(datatype=datatype, season=season)
+    for datatype, season in product(datatypes, seasons)
+}
