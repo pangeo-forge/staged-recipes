80d350741b546b44dbb85ab6de67826d583c4278
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-06-25 09:29:13 -0700

    linting

--- recipes/FESOM/recipe.py
+++ recipes/FESOM/recipe.py
@@ -1,9 +1,4 @@
-from pangeo_forge_recipes.patterns import (
-    ConcatDim,
-    FilePattern,
-    MergeDim,
-    pattern_from_file_sequence,
-)
+from pangeo_forge_recipes.patterns import ConcatDim, FilePattern, MergeDim
 from pangeo_forge_recipes.recipes import XarrayZarrRecipe
 
 months = [f"{m:02d}" for m in (2, 3, 4, 8, 9, 10)]
@@ -12,12 +7,13 @@ concat_months = ConcatDim("time_counter", keys=months)
 
 def gen_url(variable, time_counter):
     base = (
-            "https://swiftbrowser.dkrz.de/public/dkrz_035d8f6ff058403bb42f8302e6badfbc/SWOT_intercomparison/"
+        "https://swiftbrowser.dkrz.de/public/dkrz_035d8f6ff058403bb42f8302e6badfbc/"
+        "SWOT_intercomparison/"
     )
     return base + f"2012-{month}_{variable}_cubic.nc"
 
 
-surf_ocean_vars = ["dflux","ssh","sss","sst","tx_sur","ty_sur","u_surf","v_surf"]
+surf_ocean_vars = ["dflux", "ssh", "sss", "sst", "tx_sur", "ty_sur", "u_surf", "v_surf"]
 int_ocean_vars = []
 
 pattern_dict = {
