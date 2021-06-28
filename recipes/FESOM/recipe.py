1149fca7c4ecf5bf143d827b0102b6f52ee1245f
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-06-28 16:53:25 -0700

    change 'month' -> 'time'

--- recipes/FESOM/recipe.py
+++ recipes/FESOM/recipe.py
@@ -4,9 +4,9 @@ from pangeo_forge_recipes.patterns import ConcatDim, FilePattern, MergeDim
 from pangeo_forge_recipes.recipes import XarrayZarrRecipe
 
 
-def gen_url(month, variable):
+def gen_url(time, variable):
     base = "https://swift.dkrz.de/v1/dkrz_035d8f6ff058403bb42f8302e6badfbc/SWOT_intercomparison/"
-    return base + f"2012-{month}_{variable}_cubic.nc"
+    return base + f"2012-{time}_{variable}_cubic.nc"
 
 
 surf_ocean_vars = ["dflux", "ssh", "sss", "sst", "tx_sur", "ty_sur", "u_surf", "v_surf"]
@@ -25,10 +25,10 @@ def create_recipe(datatype, season, vars=var_dict, url_func=gen_url):
 
     month_range = (2, 3, 4,) if season == "fma" else (8, 9, 10,)
     months = [f"{m:02d}" for m in month_range]
-    concat_dim = ConcatDim("month", keys=months)
+    concat_dim = ConcatDim("time", keys=months)
 
     pattern = FilePattern(url_func, concat_dim, merge_dim)
-    return XarrayZarrRecipe(pattern, target_chunks={"month": 15})
+    return XarrayZarrRecipe(pattern, target_chunks={"time": 15})
 
 
 datatypes = list(var_dict)
