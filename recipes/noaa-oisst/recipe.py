05962577952d9b6797db1b540afb3a2d7343257c
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-07-01 13:00:17 -0700

    updated date range

--- recipes/noaa-oisst/recipe.py
+++ recipes/noaa-oisst/recipe.py
@@ -7,7 +7,7 @@ input_url_pattern = (
     "https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation"
     "/v2.1/access/avhrr/{yyyymm}/oisst-avhrr-v02r01.{yyyymmdd}.nc"
 )
-dates = pd.date_range("2019-09-01", "2021-01-05", freq="D")
+dates = pd.date_range("1981-09-01", "2021-06-14", freq="D")
 input_urls = [
     input_url_pattern.format(yyyymm=day.strftime("%Y%m"), yyyymmdd=day.strftime("%Y%m%d"))
     for day in dates
