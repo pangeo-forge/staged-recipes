1e2da88049909139cc65084f96bb9a680805751c
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-07-03 09:08:43 -0700

    replaced recipe with updated version

---
+++ recipes/eNATL60/recipe.py
@@ -0,0 +1,55 @@
+from itertools import product
+
+from pangeo_forge_recipes.patterns import pattern_from_file_sequence
+from pangeo_forge_recipes.recipes import XarrayZarrRecipe
+import pandas as pd
+
+
+regions = [1, 2, 3]
+season_months = {
+    'fma': pd.date_range("2010-02", "2010-05", freq="M"),
+    'aso': pd.date_range("2009-08", "2009-10", freq="M")
+}
+
+url_base = (
+    "https://ige-meom-opendap.univ-grenoble-alpes.fr"
+    "/thredds/fileServer/meomopendap/extract/SWOT-Adac"
+)
+
+
+def make_recipe_surface(region, season):
+    input_url_pattern = url_base + "/Surface/eNATL60/Region{reg:02d}-surface-hourly_{yymm}.nc"
+    months = season_months[season]
+    input_urls = [input_url_pattern.format(reg=region, yymm=date.strftime("%Y-%m"))
+                  for date in months]
+    file_pattern = pattern_from_file_sequence(input_urls, "time_counter")
+
+    recipe = XarrayZarrRecipe(
+        file_pattern,
+        target_chunks={'time_counter': 72}
+    )
+
+    return recipe
+
+
+def make_recipe_interior(region, season):
+    input_url_pattern = url_base + "/Interior/eNATL60/Region{reg:02d}-interior-daily_{yymm}.nc"    
+    months = season_months[season]
+    input_urls = [input_url_pattern.format(reg=region, yymm=date.strftime("%Y-%m"))
+                  for date in months]
+    file_pattern = pattern_from_file_sequence(input_urls, "time_counter")
+
+    recipe = XarrayZarrRecipe(
+        file_pattern,
+        target_chunks={'time_counter': "50MB"}
+    )
+    return recipe
+
+
+recipes = {f'eNATL60/Region{reg:02d}/surface_hourly/{season}': make_recipe_surface(reg, season)
+           for reg, season in product(regions, season_months)}
+
+recipes.update(
+    {f'eNATL60/Region{reg:02d}/interior_daily/{season}': make_recipe_interior(reg, season)
+     for reg, season in product(regions, season_months)}
+)
