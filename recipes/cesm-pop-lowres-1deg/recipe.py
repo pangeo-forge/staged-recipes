ddc88dabd900f50bda2e5e97425d710cfee3100b
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-07-09 10:31:46 -0700

    formatting

--- recipes/cesm-pop-lowres-1deg/recipe.py
+++ recipes/cesm-pop-lowres-1deg/recipe.py
@@ -10,32 +10,22 @@ def make_full_path(variable):
     variable: str
         A string representing each variable
     """
-    return f"https://tds.ucar.edu/thredds/fileServer/datazone/campaign/cesm/collections/ASD/v5_rel04_BC5_ne30_g16/ocn/proc/tseries/daily/v5_rel04_BC5_ne30_g16.pop.h.nday1.{variable}.00010101-01661231.nc"
+    return (
+        f"https://tds.ucar.edu/thredds/fileServer/datazone/campaign/cesm/collections/ASD/"
+        f"v5_rel04_BC5_ne30_g16/ocn/proc/tseries/daily/v5_rel04_BC5_ne30_g16.pop.h.nday1."
+        f"{variable}.00010101-01661231.nc"
+    )
 
 
 vars = [
-    'HMXL_2',
-    'SFWF_2',
-    'SHF_2',
-    'SSH_2',
-    'SSS',
-    'SST',
-    'SST2',
-    'TAUX_2',
-    'TAUY_2',
-    'U1_1',
-    'U2_2',
-    'V1_1',
-    'V2_2',
-    'XMXL_2'
+    'HMXL_2', 'SFWF_2', 'SHF_2', 'SSH_2', 'SSS', 'SST', 'SST2', 'TAUX_2', 'TAUY_2', 'U1_1',
+    'U2_2', 'V1_1', 'V2_2', 'XMXL_2',
 ]
 
 merge_dim = MergeDim("variable", keys=vars)
 
 pattern = FilePattern(make_full_path, merge_dim)
 
-chunks = {
-    'time:200'
-}
+chunks = {'time': 200}
 
 recipe = XarrayZarrRecipe(pattern, target_chunks=chunks)
