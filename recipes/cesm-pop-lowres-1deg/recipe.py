edb6452cf31adb523ca9bd294aa97b3809398e1d
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-07-09 11:05:34 -0700

    add ConcatDim

--- recipes/cesm-pop-lowres-1deg/recipe.py
+++ recipes/cesm-pop-lowres-1deg/recipe.py
@@ -1,10 +1,9 @@
-from pangeo_forge_recipes.patterns import FilePattern, MergeDim
+from pangeo_forge_recipes.patterns import ConcatDim, FilePattern, MergeDim
 from pangeo_forge_recipes.recipes import XarrayZarrRecipe
 
 
-def make_full_path(variable):
+def make_full_path(variable, time):
     """Returns a valid path to the source files
-
     Parameters
     ----------
     variable: str
@@ -13,7 +12,7 @@ def make_full_path(variable):
     return (
         f"https://tds.ucar.edu/thredds/fileServer/datazone/campaign/cesm/collections/ASD/"
         f"v5_rel04_BC5_ne30_g16/ocn/proc/tseries/daily/v5_rel04_BC5_ne30_g16.pop.h.nday1."
-        f"{variable}.00010101-01661231.nc"
+        f"{variable}.{time}.nc"
     )
 
 
@@ -34,9 +33,11 @@ vars = [
     "XMXL_2",
 ]
 
+concat_dim = ConcatDim("time", keys=["00010101-01661231",])
+
 merge_dim = MergeDim("variable", keys=vars)
 
-pattern = FilePattern(make_full_path, merge_dim)
+pattern = FilePattern(make_full_path, concat_dim, merge_dim)
 
 chunks = {"time": 200}
 
