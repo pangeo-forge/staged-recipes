8d92661b2d02eba1b2f1fff7c2467a57fc76d4a6
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-07-09 13:35:02 -0700

    linting

--- recipes/CMIP6_static_grids/recipe.py
+++ recipes/CMIP6_static_grids/recipe.py
@@ -2,10 +2,11 @@ from pangeo_forge_recipes.patterns import ConcatDim, FilePattern
 from pangeo_forge_recipes.recipes.xarray_zarr import XarrayZarrRecipe
 
 url_dict = {
-    "GFDL-ESM4":
-    "{mock_concat}ftp://ftp.gfdl.noaa.gov/perm/Alistair.Adcroft/MOM6-testing/OM4_05/ocean_hgrid.nc",
-    # "another_grid_key":
-    # "{mock_concat}ftp://another_grid_source_file_url",
+    "GFDL-ESM4": (
+        "{mock_concat}ftp://ftp.gfdl.noaa.gov/"
+        "perm/Alistair.Adcroft/MOM6-testing/OM4_05/ocean_hgrid.nc"
+        ),
+    # "another_grid_key": "{mock_concat}ftp://another_grid_source_file_url",
 }
 
 time_concat_dim = ConcatDim("mock_concat", [""], nitems_per_file=1)
@@ -38,9 +39,7 @@ def make_recipe(grid_key):
     make_full_path.__defaults__ = (grid_key,)
     filepattern = FilePattern(make_full_path, time_concat_dim)
     xarray_open_kwargs = (
-        xr_open_kwargs_dict[grid_key]
-        if grid_key in xr_open_kwargs_dict.keys()
-        else {}
+        xr_open_kwargs_dict[grid_key] if grid_key in xr_open_kwargs_dict.keys() else {}
     )
     return XarrayZarrRecipe(filepattern, process_input=pp, xarray_open_kwargs=xarray_open_kwargs)
 
