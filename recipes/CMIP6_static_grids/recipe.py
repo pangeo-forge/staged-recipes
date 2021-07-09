dd9074c9d9b8bc8a5add42f90839fd82ac7f1114
Julius Busecke <julius@ldeo.columbia.edu>
2021-07-09 11:22:02 -0400

    More pre-commit fixes

--- recipes/CMIP6_static_grids/recipe.py
+++ recipes/CMIP6_static_grids/recipe.py
@@ -2,7 +2,8 @@ from pangeo_forge_recipes.patterns import ConcatDim, FilePattern
 from pangeo_forge_recipes.recipes.xarray_zarr import XarrayZarrRecipe
 
 
-## This seems cumbersome and not elegant. Is there a way to wrap the values of `url_dict` into functions programmatically?
+# This seems cumbersome and not elegant. 
+# Is there a way to wrap the values of `url_dict` into functions programmatically?
 def gfdl_esm4_url():
     return "ftp://ftp.gfdl.noaa.gov/perm/Alistair.Adcroft/MOM6-testing/OM4_05/ocean_hgrid.nc"
 
@@ -12,7 +13,7 @@ url_dict = {"GFDL-ESM4": gfdl_esm4_url}
 
 # Preprocessors
 
-## At the moment only a dummy func
+# At the moment only a dummy func
 def gfdl_pp_func(ds):
     ds.attrs["just_a_dummy_entru"] = "again just a dummy"
     return ds
