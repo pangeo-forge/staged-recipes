3aacab271075d1476bd50142a3f97d9458353fce
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-07-09 13:24:27 -0700

    lots of edits

--- recipes/CMIP6_static_grids/recipe.py
+++ recipes/CMIP6_static_grids/recipe.py
@@ -1,35 +1,48 @@
 from pangeo_forge_recipes.patterns import ConcatDim, FilePattern
 from pangeo_forge_recipes.recipes.xarray_zarr import XarrayZarrRecipe
 
+url_dict = {
+    "GFDL-ESM4":
+    "{mock_concat}ftp://ftp.gfdl.noaa.gov/perm/Alistair.Adcroft/MOM6-testing/OM4_05/ocean_hgrid.nc",
+    # "another_grid_key":
+    # "{mock_concat}ftp://another_grid_source_file_url",
+}
 
-# This seems cumbersome and not too elegant.
-# Is there a way to wrap the values of `url_dict` into functions programmatically?
-def gfdl_esm4_url():
-    return "ftp://ftp.gfdl.noaa.gov/perm/Alistair.Adcroft/MOM6-testing/OM4_05/ocean_hgrid.nc"
+time_concat_dim = ConcatDim("mock_concat", [""], nitems_per_file=1)
 
 
-url_dict = {"GFDL-ESM4": gfdl_esm4_url}
+def make_full_path(mock_concat, grid_key="default"):
+    return url_dict[grid_key].format(mock_concat=mock_concat)
 
 
 # Preprocessors
 
-# At the moment only a dummy func
-def gfdl_pp_func(ds):
-    ds.attrs["just_a_dummy_entru"] = "again just a dummy"
-    return ds
+
+def gfdl_pp_func(ds, fname):
+    return ds.drop("tile")  # dtype="|S255" is invalid for xarray
 
 
-preprocess_dict = {"GFDL-ESM4": gfdl_pp_func}
+preprocess_dict = {
+    "GFDL-ESM4": gfdl_pp_func,
+    # "another_grid_key": "another_grid_pp_func"
+}
+
+xr_open_kwargs_dict = {
+    "GFDL-ESM4": {"engine": "scipy"},
+    # "another_grid_key": "{xarray_open_kwargs for that grid_key}"
+}
 
 
-time_concat_dim = ConcatDim("dummy", list(range(0)))
-
-
-def recipe_maker(grid_key):
+def make_recipe(grid_key):
     pp = preprocess_dict[grid_key]
-    url = url_dict[grid_key]
-    filepattern = FilePattern(url, time_concat_dim)
-    return XarrayZarrRecipe(filepattern, target_chunks={"dummy": 1}, process_intput=pp)
+    make_full_path.__defaults__ = (grid_key,)
+    filepattern = FilePattern(make_full_path, time_concat_dim)
+    xarray_open_kwargs = (
+        xr_open_kwargs_dict[grid_key]
+        if grid_key in xr_open_kwargs_dict.keys()
+        else {}
+    )
+    return XarrayZarrRecipe(filepattern, process_input=pp, xarray_open_kwargs=xarray_open_kwargs)
 
 
-recipes = {k: recipe_maker(k) for k in url_dict.keys()}
+recipes = {k: make_recipe(k) for k in url_dict.keys()}
