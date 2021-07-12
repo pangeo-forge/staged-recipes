0e814803494be7951dc141076782d6deefbc3693
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-07-12 16:11:33 -0700

    first recipe draft

---
+++ recipes/CMEMS-SLA/cmems_sla_recipe.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+from pangeo_forge_recipes.patterns import ConcatDim, FilePattern
+from pangeo_forge_recipes.recipes import XarrayZarrRecipe
+
+
+def make_full_path(date):
+    """Return a valid ftp download url based on a date input
+    """
+
+    year, month, day = date.year, date.month, date.day
+
+    return (
+        "ftp://my.cmems-du.eu/Core/SEALEVEL_GLO_PHY_L4_REP_OBSERVATIONS_008_047/"
+        f"dataset-duacs-rep-global-merged-allsat-phy-l4/{year}/{month}/"
+        f"dt_global_allsat_phy_l4_{year}{month}{day}_20190101.nc"
+    )
+
+
+dates = pd.date_range(start="1993-01-01", end="2017-05-15")
+
+concat_dim = ConcatDim("date", dates)
+
+file_pattern = FilePattern(make_full_path, concat_dim)
+
+
+def process_input(ds, fname):
+    """Drop all variables aside from `"sla"` from the dataset
+    """
+    vars_to_drop = [var for var in ds.data_vars.keys() if var != "sla"]
+    for var in vars_to_drop:
+        ds = ds.drop[var]
+    return ds
+
+
+recipe = XarrayZarrRecipe(file_pattern, process_input=process_input)
