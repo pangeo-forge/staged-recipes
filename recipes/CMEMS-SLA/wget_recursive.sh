b7bb11ee6d77f26820d7feeb6e401dbde2226b4e
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-07-12 15:09:11 -0700

    wget script (for comparison)

---
+++ recipes/CMEMS-SLA/wget_recursive.sh
@@ -0,0 +1,3 @@
+wget --user=$CMEMS_USER --password=$CMEMS_PASSWORD \
+--recursive -np -R "index.html*" \
+ftp://my.cmems-du.eu/Core/SEALEVEL_GLO_PHY_L4_REP_OBSERVATIONS_008_047/dataset-duacs-rep-global-merged-allsat-phy-l4/
