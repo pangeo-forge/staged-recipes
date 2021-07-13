9b0578ea390311438cd10c059dbe47fa5cdcc681
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-07-13 13:45:10 -0700

    download example scripts working

--- recipes/CMEMS-SLA/README.md
+++ recipes/CMEMS-SLA/README.md
@@ -1,18 +1,36 @@
-To run `wget_recursive.sh` in the `zsh` shell, first set username and password env variables:
+## Purpose
 
+The purpose of the paired scripts `wget_cmems.py` and `wget_cmems.sh` is to provide an example of a traditional "download-model" workflow, to be contrasted with a workflow centered on analysis-ready, cloud-optimized (ARCO) data.
+
+## Instructions
+
+To run `wget_cmems.py`, first set env variables:
 ```
-CMEMS_USER=<username>
-CMEMS_PASSWORD=<password>
+export CMEMS_USER=<username>
+export CMEMS_PASSWORD=<password>
+export CMEMS_DIRECTORY='my.cmems-du.eu/Core/SEALEVEL_GLO_PHY_L4_REP_OBSERVATIONS_008_047/dataset-duacs-rep-global-merged-allsat-phy-l4/'
+```
+> If you don't have an account with CMEMS, you can make one [here](https://resources.marine.copernicus.eu/?option=com_csw&task=results?option=com_csw&view=account).
+
+Clear any prexisting files, if desired:
+```
+rm -r $CMEMS_DIRECTORY*
 ```
 
-Make the script executable:
-
+Make the shell script executable:
 ```
-chmod +x wget_recursive.sh
+chmod +x wget_cmems.sh
 ```
 
-Then run the script with:
-
+Then run with:
 ```
-. ./wget_recursive.sh
+python wget_cmems.py
+```
+
+Interrupting with `Ctrl+C` will print a message such as, e.g.:
+```
+Download interrupted after 2.31 minutes (138.3 seconds).
+Of target range (1993-01-01 to 2017-05-15), 1993-01-16 was last day reached.
+Downloaded 16 of 8901 files. (~0.93 GBs of ~517.03 GBs
+Full download would take an additional ~21.33 hours (~1280.0 minutes).
 ```
---
+++ recipes/CMEMS-SLA/wget_cmems.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import time
+import subprocess
+
+from rich import print
+
+env = os.environ.copy()
+
+
+def main():
+    subprocess.run(["sh", "wget_cmems.sh"], env=env)
+
+
+if __name__ == "__main__":
+    start = time.time()
+    try:
+        main()
+    except KeyboardInterrupt:
+        elapsed = round(time.time() - start, 1)
+
+        AVG_BYTES = 58086740
+        NSOURCES = 8901
+        SOURCE_GBS = round((AVG_BYTES*NSOURCES)/1e9, 2)
+        TARGET = env["CMEMS_DIRECTORY"]
+
+        years = os.listdir(TARGET)
+        months = os.listdir(TARGET + f"/{years[-1]}")
+        files = os.listdir(TARGET + f"/{years[-1]}" + f"/{months[-1]}")
+        files = sorted([f[30:32] for f in files])
+        final_day = f"{years[-1]}-{months[-1]}-{files[-1]}"
+
+        nfiles = sum([len(files) for _, _, files in os.walk(TARGET)])
+        nbytes = round((AVG_BYTES*nfiles)/1e9, 2)
+        remaining = round(((elapsed/60)/nfiles) * (NSOURCES-nfiles), ndigits=2)
+
+        r, m, c = "[red]", "[bold magenta]", "[/]"
+        print(
+            "\n"
+            f"\n {r}Download interrupted after {c}{m}{round(elapsed/60, 2)} minutes{c}"
+            f"{r} ({round(elapsed, 1)} seconds).{c}"
+            f"\n {r}Of target range (1993-01-01 to 2017-05-15),{c} {m}{final_day}{c}"
+            f"{r} was last day reached.{c}"
+            f"\n {r}Downloaded {c}{m}{nfiles} {c}{r}of {c}{m}{NSOURCES}{c}{r} files.{c}"
+            f" {r}(~{nbytes} GBs of ~{SOURCE_GBS} GBs"
+            f"\n {r}Full download would take an additional{c}"
+            f"{m} ~{round(remaining/60, 2)} hours{c}{r} (~{remaining} minutes).{c}"
+            "\n"
+        )
+        sys.exit(0)
