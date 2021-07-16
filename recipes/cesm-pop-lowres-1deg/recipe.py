0548f8bd592a513be9acbfd4dc44f8a477af7af0
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-07-15 17:54:52 -0700

    add instantiate_recipe func

--- recipes/cesm-pop-lowres-1deg/recipe.py
+++ recipes/cesm-pop-lowres-1deg/recipe.py
@@ -2,17 +2,13 @@ from pangeo_forge_recipes.patterns import ConcatDim, FilePattern, MergeDim
 from pangeo_forge_recipes.recipes import XarrayZarrRecipe
 
 
-def make_full_path(variable, time):
+def make_full_path(variable, time, api_params=""):
     """Returns a valid path to the source files
-    Parameters
-    ----------
-    variable: str
-        A string representing each variable
     """
     return (
         f"https://tds.ucar.edu/thredds/fileServer/datazone/campaign/cesm/collections/ASD/"
         f"v5_rel04_BC5_ne30_g16/ocn/proc/tseries/daily/v5_rel04_BC5_ne30_g16.pop.h.nday1."
-        f"{variable}.{time}.nc"
+        f"{variable}.{time}.nc{api_params}"
     )
 
 
@@ -37,8 +33,12 @@ concat_dim = ConcatDim("time", keys=["00010101-01661231"])
 
 merge_dim = MergeDim("variable", keys=vars)
 
-pattern = FilePattern(make_full_path, concat_dim, merge_dim)
-
 chunks = {"time": 200}
 
-recipe = XarrayZarrRecipe(pattern, target_chunks=chunks)
+
+def instantiate_recipe(api_params):
+    
+    make_full_path.__defaults__ = (api_params,)
+    pattern = FilePattern(make_full_path, concat_dim, merge_dim)
+    
+    return XarrayZarrRecipe(pattern, target_chunks=chunks)
