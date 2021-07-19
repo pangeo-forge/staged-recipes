5eb678a2727c3653a173f4fd3c6de362a42f858b
sharkinsspatial <sharkinsgis@gmail.com>
2021-07-19 15:40:16 -0400

    Update recipe to use pangeo-forge-recipes 0.4.0 api and patterns.

--- recipes/noaa-oisst/recipe.py
+++ recipes/noaa-oisst/recipe.py
@@ -1,17 +1,21 @@
 import pandas as pd
 
-from pangeo_forge_recipes.patterns import pattern_from_file_sequence
+from pangeo_forge_recipes.patterns import ConcatDim, FilePattern
 from pangeo_forge_recipes.recipes import XarrayZarrRecipe
 
-input_url_pattern = (
-    "https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation"
-    "/v2.1/access/avhrr/{yyyymm}/oisst-avhrr-v02r01.{yyyymmdd}.nc"
-)
-dates = pd.date_range("1981-09-01", "2021-06-14", freq="D")
-input_urls = [
-    input_url_pattern.format(yyyymm=day.strftime("%Y%m"), yyyymmdd=day.strftime("%Y%m%d"))
-    for day in dates
-]
-pattern = pattern_from_file_sequence(input_urls, "time", nitems_per_file=1)
+start_date = "1981-09-01"
 
-recipe = XarrayZarrRecipe(pattern, inputs_per_chunk=20)
+
+def format_function(time):
+    base = pd.Timestamp(start_date)
+    day = base + pd.Timedelta(days=time)
+    input_url_pattern = (
+        "https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation"
+        "/v2.1/access/avhrr/{day:%Y%m}/oisst-avhrr-v02r01.{day:%Y%m%d}.nc"
+    )
+    return input_url_pattern.format(day=day)
+
+
+dates = pd.date_range(start_date, "2021-01-05", freq="D")
+pattern = FilePattern(format_function, ConcatDim("time", range(len(dates)), 1))
+recipe = XarrayZarrRecipe(pattern, inputs_per_chunk=20, cache_inputs=True)
