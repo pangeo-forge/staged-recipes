e194358c3b950c717bed186c647f2194ea01ae26
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-07-29 11:36:25 -0700

    first commit

---
+++ recipes/SODA/soda342_ice.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+from pangeo_forge_recipes.patterns import ConcatDim, FilePattern
+from pangeo_forge_recipes.recipes import XarrayZarrRecipe
+
+
+def make_full_path(time):
+    y, m, d = time.year, time.month, time.day
+    base = "https://dsrs.atmos.umd.edu/DATA/soda3.4.2/ORIGINAL/ice/"
+    filename = f"soda3.4.2_5dy_ice_or_{y}_{m:02}_{d:02}.nc"
+    return base + filename
+
+
+dates = pd.date_range("1992-01-05", "2020_12_23", freq="5D")
+
+concat_dim = ConcatDim("time", keys=dates)
+
+pattern = FilePattern(make_full_path, concat_dim)
+
+recipe = XarrayZarrRecipe(pattern, target_chunks={"time": 1})
