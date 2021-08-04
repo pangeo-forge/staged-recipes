fbb66d7ffe3a190e9e0d58c26ef7f33f4b885293
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-08-03 17:26:29 -0700

    lint

--- recipes/ORCA36/recipe.py
+++ recipes/ORCA36/recipe.py
@@ -6,12 +6,12 @@ from pangeo_forge_recipes.recipes import XarrayZarrRecipe
 
 regions = ["GulfStream", "MidAtl", "MedSea", "Agulhas"]
 regid_dict = {"GulfStream": 1, "MidAtl": 2, "MedSea": 3, "Agulhas": 4}
-varS = ['gridU', 'gridV', 'gridT']
-varI = ['gridU', 'gridV', 'gridW', 'gridT']
-varG = ['bathymetry', 'meshmask']
+varS = ["gridU", "gridV", "gridT"]
+varI = ["gridU", "gridV", "gridW", "gridT"]
+varG = ["bathymetry", "meshmask"]
 season_months = {
-        'fma': '20140201-20140430',
-        'aso': '20130801-20131031'
+        "fma": "20140201-20140430",
+        "aso": "20130801-20131031"
 }
 url_base = "ftp://ftp.mercator-ocean.fr/download/users/cbricaud/ORCA36-T404"
 
@@ -26,7 +26,7 @@ def make_recipe_surface(region, season):
 
     recipe = XarrayZarrRecipe(
         file_pattern,
-        target_chunks={'time_counter': 72}
+        target_chunks={"time_counter": 72}
     )
 
     return recipe
@@ -40,37 +40,30 @@ def make_recipe_interior(region, season):
     ]
     file_pattern = pattern_from_file_sequence(input_urls, "time_counter")
 
-    recipe = XarrayZarrRecipe(
-                    file_pattern,
-                    target_chunks={'time_counter': 1, 'y': 15},
-    )
+    recipe = XarrayZarrRecipe(file_pattern, target_chunks={"time_counter": 1, 'y': 15})
+
     return recipe
 
 
 def make_recipe_grid(region):
     input_url_pattern = "/ORCA36-T404_{var}_{reg}.nc"
-    input_urls = [
-        input_url_pattern.format(reg=region, var=variable) for variable in varG
-    ]
+    input_urls = [input_url_pattern.format(reg=region, var=variable) for variable in varG]
     file_pattern = pattern_from_file_sequence(input_urls, "mock_concat_dim", nitems_per_file=1)
 
-    recipe = XarrayZarrRecipe(
-                file_pattern,
-                target_chunks={'y': 15},
-    )
+    recipe = XarrayZarrRecipe(file_pattern, target_chunks={"y": 15})
     return recipe
 
 
 surface = {
-    f'Region{regid_dict[r]:02}/surface_hourly/{s}': make_recipe_surface(r, s)
+    f"Region{regid_dict[r]:02}/surface_hourly/{s}": make_recipe_surface(r, s)
     for r, s in product(regions, season_months)
 }
 
 interior = {
-    f'Region{regid_dict[r]:02}/interior_daily/{s}': make_recipe_interior(r, s)
+    f"Region{regid_dict[r]:02}/interior_daily/{s}": make_recipe_interior(r, s)
     for r, s in product(regions, season_months)
 }
 
-grid = {f'Region{regid_dict[r]:02}/grid': make_recipe_grid(r) for r in regions}
+grid = {f"Region{regid_dict[r]:02}/grid": make_recipe_grid(r) for r in regions}
 
 recipes = {**surface, **interior, **grid}
