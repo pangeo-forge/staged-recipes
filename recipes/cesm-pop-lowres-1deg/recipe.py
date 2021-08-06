118372b92dd35e5fc1806f86ef019ba4f092d3ef
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-08-06 13:47:14 -0700

    add nitems_per_file based on NCAR netCDF headers

--- recipes/cesm-pop-lowres-1deg/recipe.py
+++ recipes/cesm-pop-lowres-1deg/recipe.py
@@ -29,7 +29,7 @@ vars = [
     "XMXL_2",
 ]
 
-concat_dim = ConcatDim("time", keys=["00010101-01661231"])
+concat_dim = ConcatDim("time", keys=["00010101-01661231"], nitems_per_file=60590)
 
 merge_dim = MergeDim("variable", keys=vars)
 
