73ded6b78055e048c1764c6925889016e9b0d4e5
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-08-10 13:20:47 -0700

    refactor so variables use MergeDim

--- recipes/ORCA36/recipe.py
+++ recipes/ORCA36/recipe.py
@@ -1,6 +1,11 @@
 from itertools import product
 
-from pangeo_forge_recipes.patterns import pattern_from_file_sequence
+from pangeo_forge_recipes.patterns import (
+    ConcatDim,
+    FilePattern,
+    MergeDim,
+    pattern_from_file_sequence,
+)
 from pangeo_forge_recipes.recipes import XarrayZarrRecipe
 
 regions = ["GulfStream", "MidAtl", "MedSea", "Agulhas"]
@@ -10,34 +15,26 @@ varI = ["gridU", "gridV", "gridW", "gridT"]
 varG = ["bathymetry", "meshmask"]
 season_months = {"fma": "20140201-20140430", "aso": "20130801-20131031"}
 url_base = "ftp://ftp.mercator-ocean.fr/download/users/cbricaud/ORCA36-T404"
+depths = ["surface_hourly", "interior_daily"]
 
 
-def make_full_path(yymm, var, reg):
-    return url_base + f"/ORCA36-T404_1hAV_{yymm}_{var}_{reg}.nc"
+def make_recipe(region, season, depth):
 
+    vars = varS if depth == "surface_hourly" else varI
+    target_chunks = (
+        {"time_counter": 72} if depth == "surface_hourly" else {"time_counter": 1, "y": 15}
+    )
 
-def make_recipe_surface(region, season):
-    input_url_pattern = url_base + "/ORCA36-T404_1hAV_{yymm}_{var}_{reg}.nc"
+    def make_full_path(time_counter, variable, region=region):
+        return url_base + f"/ORCA36-T404_1hAV_{time_counter}_{variable}_{region}.nc"
+
     months = season_months[season]
-    input_urls = [
-        input_url_pattern.format(reg=region, yymm=months, var=variable) for variable in varS
-    ]
-    file_pattern = pattern_from_file_sequence(input_urls, "time_counter")
 
-    recipe = XarrayZarrRecipe(file_pattern, target_chunks={"time_counter": 72})
+    concat_dim = ConcatDim("time_counter", keys=months)
+    merge_dim = MergeDim("variable", keys=vars)
+    file_pattern = FilePattern(make_full_path, concat_dim, merge_dim)
 
-    return recipe
-
-
-def make_recipe_interior(region, season):
-    input_url_pattern = url_base + "/ORCA36-T404_1dAV_{yymm}_{var}_{reg}.nc"
-    months = season_months[season]
-    input_urls = [
-        input_url_pattern.format(reg=region, yymm=months, var=variable) for variable in varI
-    ]
-    file_pattern = pattern_from_file_sequence(input_urls, "time_counter")
-
-    recipe = XarrayZarrRecipe(file_pattern, target_chunks={"time_counter": 1, "y": 15})
+    recipe = XarrayZarrRecipe(file_pattern, target_chunks=target_chunks)
 
     return recipe
 
@@ -51,16 +48,11 @@ def make_recipe_grid(region):
     return recipe
 
 
-surface = {
-    f"Region{regid_dict[r]:02}/surface_hourly/{s}": make_recipe_surface(r, s)
-    for r, s in product(regions, season_months)
+recipes = {
+    f"Region{regid_dict[r]:02}/{d}/{s}": make_recipe(r, s, d)
+    for r, s, d in product(regions, season_months, depths)
 }
 
-interior = {
-    f"Region{regid_dict[r]:02}/interior_daily/{s}": make_recipe_interior(r, s)
-    for r, s in product(regions, season_months)
-}
+grids = {f"Region{regid_dict[r]:02}/grid": make_recipe_grid(r) for r in regions}
 
-grid = {f"Region{regid_dict[r]:02}/grid": make_recipe_grid(r) for r in regions}
-
-recipes = {**surface, **interior, **grid}
+recipes.update(grids)
