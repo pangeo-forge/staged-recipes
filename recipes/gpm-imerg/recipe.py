39f2c1c6219c948cba9a77b7786b9c33f69c3222
Alex Mandel <tech@wildintellect.com>
2021-09-09 08:48:41 -0700

    Switch from pattern_from_file_seq to FilePattern

--- recipes/gpm-imerg/recipe.py
+++ recipes/gpm-imerg/recipe.py
@@ -1,31 +1,28 @@
 from pangeo_forge_recipes.recipes import XarrayZarrRecipe
-from pangeo_forge_recipes.patterns import pattern_from_file_sequence
+from pangeo_forge_recipes.patterns import ConcatDim, FilePattern
 import pandas as pd
 
 #TODO: replace with ENV vars
 username = password = 'pangeo@developmentseed.org'
 
-dates = pd.date_range("2000-06-01T00:00:00", "2021-05-31T23:59:59", freq="30min")
-input_url_pattern = (
-    "https://arthurhouhttps.pps.eosdis.nasa.gov/gpmdata/{yyyy}/{mm}/{dd}/imerg/3B-HHR.MS.MRG.3IMERG.{yyyymmdd}-S{sh}{sm}00-E{eh}{em}59.{MMMM}.V06B.HDF5"
-)
-
-input_urls = [
-    input_url_pattern.format(
-        yyyy=hhr.strftime("%Y"),
-        mm = hhr.strftime("%m"),
-        dd = hhr.strftime("%d"),
-        yyyymmdd=hhr.strftime("%Y%m%d"),
-        sh = hhr.strftime("%H"),
-        sm = hhr.strftime("%M"),
-        eh = hhr.strftime("%H"),
-        em = (hhr+pd.Timedelta("29 min")).strftime("%M"),
-        MMMM = f'{(hhr.hour*60 + hhr.minute):04}'
+def make_filename(time):
+    input_url_pattern = (
+        "https://arthurhouhttps.pps.eosdis.nasa.gov/gpmdata/{yyyy}/{mm}/{dd}/imerg/3B-HHR.MS.MRG.3IMERG.{yyyymmdd}-S{sh}{sm}00-E{eh}{em}59.{MMMM}.V06B.HDF5"
+    ).format(
+        yyyy=time.strftime("%Y"),
+        mm = time.strftime("%m"),
+        dd = time.strftime("%d"),
+        yyyymmdd=time.strftime("%Y%m%d"),
+        sh = time.strftime("%H"),
+        sm = time.strftime("%M"),
+        eh = time.strftime("%H"),
+        em = (time+pd.Timedelta("29 min")).strftime("%M"),
+        MMMM = f'{(time.hour*60 + time.minute):04}'
     )
-    for hhr in dates
-]
+    return input_url_pattern
 
-pattern = pattern_from_file_sequence(input_urls, "time", nitems_per_file=1)
+time_concat_dim = ConcatDim("time", dates, nitems_per_file=1)
+pattern = FilePattern(make_filename, time_concat_dim)
 
 recipe = XarrayZarrRecipe(
     pattern, 
