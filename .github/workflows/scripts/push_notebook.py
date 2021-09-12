82200354cf79c16440d6ffcee10b38a1f70ab50f
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-09-11 18:10:06 -0700

    add push_notebook script with lint

---
+++ .github/workflows/scripts/push_notebook.py
@@ -0,0 +1,10 @@
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    print(
+        f"TODO: Populate a .ipynb jinja template with path: {args[-1]}\n"
+        "TODO: Execute notebook with nbcovert cli?"
+        f"TODO: Push notebook to pangeo-forge/recipe-test-notebooks using {args[:-1]} "
+        "for directory and filename args."
+    )
