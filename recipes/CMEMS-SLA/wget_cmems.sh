6815fdbc23e0b0059fc5c212fb8d8585e94889a6
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-09-24 10:46:28 -0700

    remove extra files

--- recipes/CMEMS-SLA/wget_cmems.sh
+++
@@ -1,3 +0,0 @@
-wget --user=$CMEMS_USER --password=$CMEMS_PASSWORD \
---recursive -np -R "index.html*" \
-ftp://my.cmems-du.eu/Core/SEALEVEL_GLO_PHY_L4_REP_OBSERVATIONS_008_047/dataset-duacs-rep-global-merged-allsat-phy-l4/
