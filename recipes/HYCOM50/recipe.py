07026f50040268fe7532dd9d227621a1c6d7ba0e
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-10-18 18:54:33 -0700

    add vertical velocities

--- recipes/HYCOM50/recipe.py
+++ recipes/HYCOM50/recipe.py
@@ -4,7 +4,7 @@ from pangeo_forge_recipes.recipes import XarrayZarrRecipe
 regs = ["GS", "GE", "MD"]
 
 
-def gen_urls(reg, season=None, datatype=None, grid=False):
+def gen_urls(reg, season=None, datatype=None, grid=False, velocity=False):
     """
 
     Parameters
@@ -22,7 +22,8 @@ def gen_urls(reg, season=None, datatype=None, grid=False):
         assert datatype, "If `grid=False`, `datatype` must be one of `surf` or `int`."
         months = ["Feb", "Mar", "Apr"] if season == "fma" else ["Aug", "Sep", "Oct"]
         freq = "hourly" if datatype == "surf" else "daily"
-        urls = [base + f"HYCOM50_E043_{month}_{reg}_{freq}.nc" for month in months]
+        velocity_suffix = "_wvel" if velocity else ""
+        urls = [base + f"HYCOM50_E043_{month}_{reg}_{freq}{velocity_suffix}.nc" for month in months]
     else:
         urls = [
             base + f"HYCOM50_grid_{reg}.nc",
@@ -31,7 +32,7 @@ def gen_urls(reg, season=None, datatype=None, grid=False):
     return urls
 
 
-def create_recipes(datatype, season, regs=regs):
+def create_recipes(datatype, season, regs=regs, velocity=False):
     """
     Parameters
     ----------
@@ -46,15 +47,23 @@ def create_recipes(datatype, season, regs=regs):
     -------
     A dictionary of recipes.
     """
+    velocity_suffix = "_wvel" if velocity else ""
     patterns = {
-        f"HYCOM50/Region{i+1:02}_{reg}/{datatype}/{season}": pattern_from_file_sequence(
-            gen_urls(season=season, reg=reg, datatype=datatype), concat_dim="time",
+        f"HYCOM50/Region{i+1:02}_{reg}/{datatype}{velocity_suffix}/{season}": pattern_from_file_sequence(
+            gen_urls(season=season, reg=reg, datatype=datatype, velocity=velocity,),
+            concat_dim="time",
         )
         for i, reg in zip(range(3), regs)
     }
+    subset_inputs = {"time": 3} if velocity else {}
+    target_chunks = {"time": 4} if velocity else {"time": 24}
     recipes = {
         list(patterns)[i]: (
-            XarrayZarrRecipe(patterns[list(patterns)[i]], target_chunks={"time": 24},)
+            XarrayZarrRecipe(
+                patterns[list(patterns)[i]],
+                target_chunks=target_chunks,
+                subset_inputs=subset_inputs,
+            )
         )
         for i in range(3)
     }
@@ -78,5 +87,7 @@ recipes = {
     **create_recipes("surf", "aso"),
     **create_recipes("int", "fma"),
     **create_recipes("int", "aso"),
+    **create_recipes("int", "fma", velocity=True),
+    **create_recipes("int", "aso", velocity=True),
     **grid_recipes,
 }
