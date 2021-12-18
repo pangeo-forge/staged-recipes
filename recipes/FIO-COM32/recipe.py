4cbe5779a49ff8133cc9b23d591cb494e81fbb5d
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2021-12-17 20:17:19 -0800

    update recipe to merge vars + adjust chunking

--- recipes/FIO-COM32/recipe.py
+++ recipes/FIO-COM32/recipe.py
@@ -1,7 +1,6 @@
 from itertools import product
 
-import pandas as pd
-from pangeo_forge_recipes.patterns import pattern_from_file_sequence
+from pangeo_forge_recipes.patterns import ConcatDim, FilePattern, MergeDim
 from pangeo_forge_recipes.recipes import XarrayZarrRecipe
 
 regions = [1, 2, 3, 4]
@@ -11,58 +10,69 @@ surf_vars = ["salt", "temp", "ssh", "u", "v"]
 surf_flux = ["pme", "heat_tot", "taux", "tauy"]
 int_vars = ["salt", "temp", "u", "v", "w"]
 
-url_base = (
-        "sftp://swotadac@221.0.186.42:/shared/SWOT-AdAC/"
-            )
+url_base = "sftp://swotadac@221.0.186.42:/shared/SWOT-AdAC/"
 
 
-def make_recipe_surface(region, season, variable):
-    input_url_pattern = url_base + "Region{reg:02d}-hourly-{var}.{seas}.nc"
-    input_urls = [
-            input_url_pattern.format(reg=region, var=variable, seas=season)
-                ]
-    file_pattern = pattern_from_file_sequence(input_urls, "time")
+def make_recipe(
+    region, season, variables, freq, target_chunks, subset_inputs,
+):
+    def make_full_path(time, variable):
+        return url_base + f"Region{region:02d}-{freq}-{variable}.{season}{time}.nc"
 
-    target_chunks = {"time": 72}
-    subset_inputs = {"time": 3}
+    # Until we support no concat dim, we need an empty string placeholder. Issue:
+    # https://github.com/pangeo-forge/pangeo-forge-recipes/issues/212
+    concat_dim = ConcatDim("time", keys=[""])
+    merge_dim = MergeDim("variable", keys=variables)
+    file_pattern = FilePattern(make_full_path, concat_dim, merge_dim)
+
     recipe = XarrayZarrRecipe(
-                    file_pattern, target_chunks=target_chunks, subset_inputs=subset_inputs
-                             )
-
+        file_pattern, target_chunks=target_chunks, subset_inputs=subset_inputs,
+    )
     return recipe
 
 
-def make_recipe_interior(region, season, variable):
-    input_url_pattern = url_base + "Region{reg:02d}-daily-{var}.{seas}.nc"
-    input_urls = [
-            input_url_pattern.format(reg=region, var=variable, seas=season)
-                ]
-    file_pattern = pattern_from_file_sequence(input_urls, "time")
+surf_target_chunks = {"time": 275}
+surf_subset_inputs = {"time": 2}
 
-    target_chunks = {"time": 1}
-    subset_inputs = {"time": 30}
-    recipe = XarrayZarrRecipe(
-                    file_pattern, target_chunks=target_chunks, subset_inputs=subset_inputs
-                        )
-    
-    return recipe
-
+int_target_chunks = {"time": 5}
+int_subset_inputs = {"time": 4}
 
 recipes = {
-         f"FIO-COM32/Region{reg:02d}/surface_hourly/{seas}": make_recipe_surface(reg, seas, var)
-         for reg, seas, var in product(regions, seasons, surf_vars)
+    f"FIO-COM32/Region{reg:02d}/surface_hourly/{seas.lower()}": make_recipe(
+        reg,
+        seas,
+        variables=surf_vars,
+        freq="hourly",
+        target_chunks=surf_target_chunks,
+        subset_inputs=surf_subset_inputs,
+    )
+    for reg, seas in product(regions, seasons)
 }
 
 recipes.update(
     {
-         f"FIO-COM32/Region{reg:02d}/surface_flux_hourly/{seas}": make_recipe_surface(reg, seas, var)
-         for reg, seas, var in product(regions, seasons, surf_flux)
+        f"FIO-COM32/Region{reg:02d}/surface_flux_hourly/{seas.lower()}": make_recipe(
+            reg,
+            seas,
+            variables=surf_flux,
+            freq="hourly",
+            target_chunks=surf_target_chunks,
+            subset_inputs=surf_subset_inputs,
+        )
+        for reg, seas in product(regions, seasons)
     }
 )
 
 recipes.update(
     {
-         f"FIO-COM32/Region{reg:02d}/interior_daily/{seas}": make_recipe_interior(reg, seas, var)
-         for reg, seas, var in product(regions, seasons, int_vars)
+        f"FIO-COM32/Region{reg:02d}/interior_daily/{seas.lower()}": make_recipe(
+            reg,
+            seas,
+            variables=int_vars,
+            freq="daily",
+            target_chunks=int_target_chunks,
+            subset_inputs=int_subset_inputs,
+        )
+        for reg, seas in product(regions, seasons)
     }
 )
