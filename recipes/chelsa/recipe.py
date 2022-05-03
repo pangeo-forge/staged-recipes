1060a982e30ef3651f265273a9f9197d21501172
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-05-03 10:36:39 -0700

    add xarray_open_kwargs back in, which were somehow lost in revert

--- recipes/chelsa/recipe.py
+++ recipes/chelsa/recipe.py
@@ -36,7 +36,6 @@ pattern = FilePattern(
 
 recipe = XarrayZarrRecipe(
     file_pattern=pattern,
-    inputs_per_chunk=1 
+    inputs_per_chunk=1,
+    xarray_open_kwargs={"engine": "rasterio"},
 )
-
-run_function = recipe.to_function()
