0fc531f2e00467cd98c6eb9482533d8312f8d8a4
Will Gregory <William.gregory@outlook.com>
2022-06-13 14:40:59 -0400

    Update recipe.py

--- recipes/SPEAR_SICDA_forecasts_F02/recipe.py
+++ recipes/SPEAR_SICDA_forecasts_F02/recipe.py
@@ -6,7 +6,7 @@ dates = [str(x) for x in range(1982,2018)]
 def make_url(time):
     return "ftp://ftp.gfdl.noaa.gov/pub/William.Gregory/SICDA_forecasts_F02/"+time+".ice_daily.ens_mean.nc"
 
-time_concat_dim = ConcatDim("time", dates, nitems_per_file=365)
+time_concat_dim = ConcatDim("time", dates)
 pattern = FilePattern(make_url, time_concat_dim)
 
-recipe = XarrayZarrRecipe(pattern, inputs_per_chunk=1)
+recipe = XarrayZarrRecipe(pattern, target_chunks={'time':180,'xT':10,'yT':10})
