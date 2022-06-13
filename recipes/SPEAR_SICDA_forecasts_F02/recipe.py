da04c20a092e67e22256b7d82bfe245f25465217
Will Gregory <William.gregory@outlook.com>
2022-06-13 11:41:32 -0400

    Update recipe.py

--- recipes/SPEAR_SICDA_forecasts_F02/recipe.py
+++ recipes/SPEAR_SICDA_forecasts_F02/recipe.py
@@ -4,7 +4,7 @@ from pangeo_forge_recipes.recipes import XarrayZarrRecipe
 dates = [str(x) for x in range(1982,2018)]
 
 def make_url(time):
-    return "ftp://ftp.gfdl.noaa.gov/pub/William.Gregory/SIC_forecasts_F02/"+time+".ice_daily.ens_mean.nc"
+    return "ftp://ftp.gfdl.noaa.gov/pub/William.Gregory/SICDA_forecasts_F02/"+time+".ice_daily.ens_mean.nc"
 
 time_concat_dim = ConcatDim("time", dates, nitems_per_file=365)
 pattern = FilePattern(make_url, time_concat_dim)
