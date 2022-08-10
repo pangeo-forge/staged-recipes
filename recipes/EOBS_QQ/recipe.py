54f8fbe044a22df47bf5fec65402680399bebfc8
Raphael Hagen <norlandrhagen@gmail.com>
2022-08-10 09:57:19 -0700

    EOBS surface_downwelling_shortwave_flux_in_air

---
+++ recipes/EOBS_QQ/recipe.py
@@ -0,0 +1,26 @@
+"""Author: Norland Raphael Hagen - 08-09-2022
+Pangeo-Forge recipe for E-OBS QQ data (E-OBS: High-resolution surface downwelling shortwave flux in air for Europe & Northern Africa)"""  # noqa: E501
+
+
+from pangeo_forge_recipes.patterns import ConcatDim, FilePattern, MergeDim
+from pangeo_forge_recipes.recipes import XarrayZarrRecipe
+
+# Filename Pattern Inputs
+target_chunks = {"time": 40}
+dataset_version = "v23.1e"
+grid_res = "0.1"
+variables = ["qq"]
+dataset_version = "v23.1e"
+grid_res = "0.1"
+
+def make_filename(time: str, variable: str) -> str:
+    return f"https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/Grid_{grid_res}deg_reg_ensemble/{variable}_ens_mean_{grid_res}deg_reg_{dataset_version}.nc"  # noqa: E501
+
+pattern = FilePattern(
+    make_filename, ConcatDim("time", keys=[""]), MergeDim(name="variable", keys=variables)
+)
+
+recipe = XarrayZarrRecipe(
+    file_pattern=pattern, target_chunks=target_chunks,
+    subset_inputs={"time": 700}
+)
