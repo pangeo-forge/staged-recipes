d92d9c060e2e8a213e13a94f5d9c8c4c0b25e2ac
jlanders <jordan.landers+dev@gmail.com>
2022-08-28 00:00:48 -0700

    C-iTrace materials

---
+++ recipes/C-iTRACE/recipe.py
@@ -0,0 +1,24 @@
+from pangeo_forge_recipes.patterns import ConcatDim, FilePattern, MergeDim
+from pangeo_forge_recipes.recipes.xarray_zarr import XarrayZarrRecipe
+
+# C-iTRACE covers a number of proxies
+variables = ['d18O']
+
+def make_url(time, variable):
+    url = 'https://gdex.ucar.edu/dataset/204_ajahn/file/ctrace.decadal.{variable}.nc'.format(variable=variable)
+    return url
+
+# there is only one, but notably ~6 Gb file covering the full timeseries
+time_concat_dim = ConcatDim("time", [0], nitems_per_file=2200)
+pattern = FilePattern(make_url,
+                      time_concat_dim,
+                      MergeDim(name="variable", keys=variables))
+
+
+# Create recipe object
+# use subset_inputs to make the processing more tractable
+recipe = XarrayZarrRecipe(pattern, inputs_per_chunk=1,
+                          consolidate_zarr=False,
+                          subset_inputs={'time':220},
+                          copy_input_to_local_file=True,
+                          xarray_open_kwargs={'decode_coords':True})
