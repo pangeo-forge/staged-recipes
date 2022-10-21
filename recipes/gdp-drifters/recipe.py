867863cebc067a3eb428551685ce7cdc3e75342e
Anderson Banihirwe <axbanihirwe@gmail.com>
2022-10-20 19:35:15 -0600

    remove unused import

--- recipes/gdp-drifters/recipe.py
+++ recipes/gdp-drifters/recipe.py
@@ -1,4 +1,4 @@
-from pangeo_forge_recipes.patterns import FileType, pattern_from_file_sequence
+from pangeo_forge_recipes.patterns import pattern_from_file_sequence
 from pangeo_forge_recipes.recipes import XarrayZarrRecipe
 
 url = 'https://www.nodc.noaa.gov/archive/arc0199/0248584/1.1/data/0-data/gdp_v2.00.nc'
