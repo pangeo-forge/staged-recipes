3602a4c75975dadbe5513d2477411f3b40c88cd0
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2022-11-14 19:49:35 +0100

    run pre-commit

--- recipes/cru-ts/recipe.py
+++ recipes/cru-ts/recipe.py
@@ -1,18 +1,19 @@
-# The authentication is directly copied from https://github.com/cedadev/opendap-python-example/blob/master/simple_file_downloader.py
+# The authentication is directly copied from:
 #
-import os
+#   https://github.com/cedadev/opendap-python-example/blob/master/simple_file_downloader.py
+#
 import datetime
+import os
 import ssl
-from getpass import getpass
-
-
-# Import third-party libraries
-from cryptography import x509
-from cryptography.hazmat.backends import default_backend
 
 # from contrail.security.online_ca_client import OnlineCaClient
 # pip install ContrailOnlineCAClient
 from contrail.security.onlineca.client import OnlineCaClient
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+
+from pangeo_forge_recipes.patterns import ConcatDim, FilePattern, MergeDim
+from pangeo_forge_recipes.recipes import XarrayZarrRecipe
 
 # Credentials defaults
 DODS_FILE_CONTENTS = """HTTP.COOKIEJAR=./dods_cookies
@@ -41,7 +42,9 @@ def write_dods_file_contents():
     HTTP.SSL.CERTIFICATE={credentials_file_path}
     HTTP.SSL.KEY={credentials_file_path}
     HTTP.SSL.CAPATH={trustroots_dir}
-    """.format(credentials_file_path=CREDENTIALS_FILE_PATH, trustroots_dir=TRUSTROOTS_DIR)
+    """.format(
+        credentials_file_path=CREDENTIALS_FILE_PATH, trustroots_dir=TRUSTROOTS_DIR
+    )
 
     with open(DODS_FILE_PATH, 'w') as dods_file:
         dods_file.write(DODS_FILE_CONTENTS)
@@ -59,7 +62,7 @@ def cert_is_valid(cert_file, min_lifetime=0):
     try:
         with open(cert_file, 'rb') as f:
             crt_data = f.read()
-    except IOError:
+    except OSError:
         return False
 
     try:
@@ -69,11 +72,11 @@ def cert_is_valid(cert_file, min_lifetime=0):
 
     now = datetime.datetime.now()
 
-    return (cert.not_valid_before <= now
-            and cert.not_valid_after > now + datetime.timedelta(0, min_lifetime))
-    
+    return cert.not_valid_before <= now and cert.not_valid_after > now + datetime.timedelta(
+        0, min_lifetime
+    )
 
-    
+
 def setup_credentials(force=False):
     """
     Download and create required credentials files.
@@ -92,23 +95,17 @@ def setup_credentials(force=False):
     onlineca_client.ca_cert_dir = TRUSTROOTS_DIR
 
     # Set up trust roots
-    trustroots = onlineca_client.get_trustroots(
-        TRUSTROOTS_SERVICE,
-        bootstrap=True,
-        write_to_ca_cert_dir=True)
-    
-    #username = input("CEDA username")
-    #password = getpass("CEDA password")
+    onlineca_client.get_trustroots(TRUSTROOTS_SERVICE, bootstrap=True, write_to_ca_cert_dir=True)
+
+    # username = input("CEDA username")
+    # password = getpass("CEDA password")
     username = os.environ['CEDA_USERNAME']
     password = os.environ['CEDA_PASSWORD']
 
-
     # Write certificate credentials file
     key_pair, certs = onlineca_client.get_certificate(
-        username,
-        password,
-        CERT_SERVICE,
-        pem_out_filepath=CREDENTIALS_FILE_PATH)
+        username, password, CERT_SERVICE, pem_out_filepath=CREDENTIALS_FILE_PATH
+    )
 
     # Write the dodsrc credentials file
     write_dods_file_contents()
@@ -117,13 +114,7 @@ def setup_credentials(force=False):
     return CREDENTIALS_FILE_PATH
 
 
-
-
-
-### +++++++++  here comes the actual recipe definition +++++++++++++++++++++++
-from pangeo_forge_recipes.patterns import ConcatDim, FilePattern, MergeDim
-from pangeo_forge_recipes.recipes import XarrayZarrRecipe
-from pangeo_forge_recipes.recipes import setup_logging
+# +++++++++  here comes the actual recipe definition +++++++++++++++++++++++
 
 
 def get_ssl():
@@ -132,17 +123,25 @@ def get_ssl():
     return sslcontext
 
 
-def make_url(time, variable, version="4.05"):
-    ## there is some peculiarities with 4.06 where the file pattern has an exception for cld
-    ## the version for cld is 4.06.01 and 4.06 for all other variables...
-    return f'https://dap.ceda.ac.uk/badc/cru/data/cru_ts/cru_ts_{version}/data/{variable}/cru_ts{version}.1901.1910.{variable}.dat.nc.gz'
+def make_url(time, variable, version='4.05'):
+    # there is some peculiarities with 4.06 where the file pattern has an exception for cld
+    # the version for cld is 4.06.01 and 4.06 for all other variables...
+    return (
+        f'https://dap.ceda.ac.uk/badc/cru/data/cru_ts/cru_ts_{version}/'
+        'data/{variable}/cru_ts{version}.1901.2020.{variable}.dat.nc.gz'
+    )
 
-# This is inspired by the EOBS feedstock: https://github.com/pangeo-forge/EOBS-feedstock/blob/main/feedstock/recipe.py
+
+# This is inspired by the EOBS feedstock:
+# https://github.com/pangeo-forge/EOBS-feedstock/blob/main/feedstock/recipe.py
 pattern = FilePattern(
     make_url,
     ConcatDim('time', keys=['']),
-    MergeDim(name='variable', keys=["cld", "dtr", "frs", "pet", "pre", "tmn", "tmp", "tmx", "vap", "wet"]),
-    fsspec_open_kwargs={'compression':'gzip', 'ssl': get_ssl()}, file_type="netcdf3"
+    MergeDim(
+        name='variable', keys=['cld', 'dtr', 'frs', 'pet', 'pre', 'tmn', 'tmp', 'tmx', 'vap', 'wet']
+    ),
+    fsspec_open_kwargs={'compression': 'gzip', 'ssl': get_ssl()},
+    file_type='netcdf3',
 )
 
 
