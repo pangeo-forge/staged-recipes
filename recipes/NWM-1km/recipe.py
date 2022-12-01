b9bf0ec8bac8a8a0ca6cbaa86e5c1fbe846d47b9
Rich Signell <rsignell@usgs.gov>
2022-12-01 16:41:09 -0500

    Create recipe.py

---
+++ recipes/NWM-1km/recipe.py
@@ -0,0 +1,22 @@
+import fsspec
+import kerchunk
+
+from pangeo_forge_recipes.patterns import pattern_from_file_sequence
+from pangeo_forge_recipes.recipes.reference_hdf_zarr import HDFReferenceRecipe
+
+fs = fsspec.filesystem(
+    's3', anon=True
+)
+
+all_paths = sorted(fs.glob('noaa-nwm-retrospective-2-1-pds/model_output/2017/*LDAS*''))
+
+pattern = pattern_from_file_sequence(['s3://' + path for path in all_paths], 'time')
+
+
+recipe = HDFReferenceRecipe(
+    pattern,
+    netcdf_storage_options={
+        'anon': True
+    },
+    identical_dims=['x','y'], preprocess=kerchunk.combine.drop('reference_time')
+)
