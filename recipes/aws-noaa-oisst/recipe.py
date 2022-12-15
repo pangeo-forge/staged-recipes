3977e854da965ab126777b1b720e272d7474d6a7
Ryan Avery <ryanbarryavery@gmail.com>
2022-12-15 14:58:04 -0800

    import pandas top level

--- recipes/aws-noaa-oisst/recipe.py
+++ recipes/aws-noaa-oisst/recipe.py
@@ -1,8 +1,8 @@
+import pandas as pd
 from pangeo_forge_recipes.patterns import ConcatDim, FilePattern
 from pangeo_forge_recipes.recipes.reference_hdf_zarr import HDFReferenceRecipe
 
 start_date = '1981-09-01'
-
 
 def format_function(time):
     """Returns the url to an item for a time based on a dataset specific url pattern.
@@ -25,7 +25,6 @@ def format_function(time):
         '/v2.1/avhrr/{day:%Y%m}/oisst-avhrr-v02r01.{day:%Y%m%d}.nc'
     )
     return input_url_pattern.format(day=day)
-
 
 dates = pd.date_range(start_date, '2022-11-08', freq='D')
 pattern = FilePattern(format_function, ConcatDim('time', range(len(dates)), nitems_per_file=1))
