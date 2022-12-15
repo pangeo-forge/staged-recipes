85556ef8d5095c5a9f557c1bfad6db038f93fa7d
Rich Signell <rsignell@usgs.gov>
2022-12-15 16:07:50 -0500

    new recipe for rechunking

--- recipes/NWM-1km/recipe.py
+++ recipes/NWM-1km/recipe.py
@@ -1,22 +1,23 @@
-import fsspec
-import kerchunk
-
-from pangeo_forge_recipes.patterns import pattern_from_file_sequence
-from pangeo_forge_recipes.recipes.reference_hdf_zarr import HDFReferenceRecipe
-
-fs = fsspec.filesystem(
-    's3', anon=True
-)
-
-all_paths = sorted(fs.glob('noaa-nwm-retrospective-2-1-pds/model_output/*/*LDAS*'))
-
-pattern = pattern_from_file_sequence(['s3://' + path for path in all_paths], 'time')
+import pandas as pd
+from pangeo_forge_recipes.patterns import ConcatDim
+from pangeo_forge_recipes.patterns import FilePattern
+from pangeo_forge_recipes.recipes import XarrayZarrRecipe
 
 
-recipe = HDFReferenceRecipe(
-    pattern,
-    netcdf_storage_options={
-        'anon': True
-    },
-    identical_dims=['x','y'], preprocess=kerchunk.combine.drop('reference_time')
-)
+dates = pd.date_range('1979-02-01 03:00', '2020-12-31 21:00', freq='3H')
+
+time_concat_dim = ConcatDim("time", dates, nitems_per_file=1)
+
+URL_FORMAT = ('s3://noaa-nwm-retrospective-2-1-pds/model_output/{time:%Y}/{time:%Y%m%d%H}00.LDASOUT_DOMAIN1.comp')
+
+def make_url(time):
+    return URL_FORMAT.format(time=time)
+
+pattern = FilePattern(make_url, time_concat_dim)
+
+target_chunks = {'time':72, 'x':512, 'y':512}
+recipe = XarrayZarrRecipe(pattern, target_chunks=target_chunks)
+
+run_function = recipe_pruned.to_function()
+
+run_function()
