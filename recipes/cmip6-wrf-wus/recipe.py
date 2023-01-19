949134a52737d1440b8a020b35dfec95f26f9552
Anderson Banihirwe <axbanihirwe@ualr.edu>
2023-01-19 00:07:47 -0700

    use https instead of s3

--- recipes/cmip6-wrf-wus/recipe.py
+++ recipes/cmip6-wrf-wus/recipe.py
@@ -8,8 +8,8 @@ from pangeo_forge_recipes.recipes import HDFReferenceRecipe
 recipe_dict = {
     'cesm2_r11i1p1f1_ssp370': {
         'url_format': (
-            's3://wrf-cmip6-noversioning/downscaled_products/gcm/cesm2_r11i1p1f1_ssp370/'
-            'hourly/{Time:%Y}/d02/auxhist_d01_{Time:%Y-%m-%d_%X}'
+            'https://wrf-cmip6-noversioning.s3.amazonaws.com/downscaled_products/gcm/'
+            'cesm2_r11i1p1f1_ssp370/hourly/{Time:%Y}/d02/auxhist_d01_{Time:%Y-%m-%d_%X}'
         ),
         'times': pd.date_range('2014-09-01', '2100-08-31 23:00', freq='H'),
     }
