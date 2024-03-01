0aa8e6bf30930c512d480da1a9e1ba25a4d93be0
Raphael Hagen <norlandrhagen@gmail.com>
2024-02-29 17:21:49 -0700

    squash

---
+++ recipes/feedstock/config.py
@@ -0,0 +1,14 @@
+# vi local-runner-config.py
+c.Bake.bakery_class = "pangeo_forge_runner.bakery.local.LocalDirectBakery"
+c.Bake.prune = True
+c.LocalDirectBakery.num_workers = 1
+
+c.MetadataCacheStorage.fsspec_class = "fsspec.implementations.local.LocalFileSystem"
+# Metadata cache should be per `{{job_name}}`, as kwargs changing can change metadata
+c.MetadataCacheStorage.root_path = "local_storage/metadatacache/"
+
+c.TargetStorage.fsspec_class = "fsspec.implementations.local.LocalFileSystem"
+c.TargetStorage.root_path = "local_storage/target/"
+
+c.InputCacheStorage.fsspec_class = "fsspec.implementations.local.LocalFileSystem"
+c.InputCacheStorage.root_path = "local_storage/cache/"
