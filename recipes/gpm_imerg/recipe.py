e182463a74b34cbb32a78b536b0395edfe120b91
sudobangbang <gregcorradini@gmail.com>
2024-03-08 02:51:00 -0800

    Update recipe.py

--- recipes/gpm_imerg/recipe.py
+++ recipes/gpm_imerg/recipe.py
@@ -13,7 +13,7 @@ from pangeo_forge_recipes.transforms import OpenWithKerchunk, WriteCombinedRefer
 
 ED_USERNAME = os.environ['EARTHDATA_USERNAME']
 ED_PASSWORD = os.environ['EARTHDATA_PASSWORD']
-earthdata_protocol = os.environ['PROTOCOL'] or 'https'
+earthdata_protocol = os.environ.get('PROTOCOL') or 'https'
 
 if earthdata_protocol not in ('https', 's3'):
     raise ValueError(f'Unknown ED_PROTOCOL: {earthdata_protocol}')
