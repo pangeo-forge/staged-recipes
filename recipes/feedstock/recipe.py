9426a15135b2e0e301633c1ef664845ddb21040b
sudobangbang <gregcorradini@gmail.com>
2024-03-07 16:57:42 -0800

    6 months and multiple attrs

--- recipes/feedstock/recipe.py
+++ recipes/feedstock/recipe.py
@@ -40,7 +40,7 @@ IDENTICAL_DIMS = ['lat', 'lon']
 # 2023/07/3B-DAY.MS.MRG.3IMERG.20230731
 dates = [
     d.to_pydatetime().strftime('%Y/%m/3B-DAY.MS.MRG.3IMERG.%Y%m%d')
-    for d in pd.date_range('2000-06-01', '2000-06-03', freq='D')
+    for d in pd.date_range('2000-06-01', '2000-12-01', freq='D')
 ]
 #2014-01-01
 
@@ -125,7 +125,7 @@ class DropVarCoord(beam.PTransform):
         index, ds = item
         # Removing time_bnds since it doesn't have spatial dims
         ds = ds.drop_vars('time_bnds')
-        ds = ds[['precipitation']]
+        #ds = ds[['precipitation']]
 
         # ds = ds[['precipitation','MWprecipitation','randomError']]
         return index, ds
