e1723f29725febbcb006d24a580a0a3b43447d3b
Raphael Hagen <norlandrhagen@gmail.com>
2024-03-13 15:42:56 -0600

    pyr, 1 year, https

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -186,19 +186,19 @@ recipe = (
     | OpenWithXarray(file_type=pattern.file_type)
     | TransposeCoords()
     | DropVarCoord()
-    | StoreToZarr(
-        store_name=SHORT_NAME,
-        combine_dims=pattern.combine_dim_keys,
-    )    
-    # | 'Write Pyramid Levels'
-    # >> StoreToPyramid(
+    # | StoreToZarr(
     #     store_name=SHORT_NAME,
-    #     epsg_code='4326',
-    #     rename_spatial_dims={'lon': 'longitude', 'lat': 'latitude'},
-    #     n_levels=2,
-    #     pyramid_kwargs={'extra_dim': 'nv'},
     #     combine_dims=pattern.combine_dim_keys,
-    # )
+    # )    
+    | 'Write Pyramid Levels'
+    >> StoreToPyramid(
+        store_name=SHORT_NAME,
+        epsg_code='4326',
+        rename_spatial_dims={'lon': 'longitude', 'lat': 'latitude'},
+        n_levels=2,
+        pyramid_kwargs={'extra_dim': 'nv'},
+        combine_dims=pattern.combine_dim_keys,
+    )
 )
 
 # recipe = (
