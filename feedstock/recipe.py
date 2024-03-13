972fa07a85c16aab5aeef6b74f4e653619a718b1
Raphael Hagen <norlandrhagen@gmail.com>
2024-03-13 11:15:54 -0600

    3 mo

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -48,7 +48,7 @@ IDENTICAL_DIMS = ['lat', 'lon']
 # 2023/07/3B-DAY.MS.MRG.3IMERG.20230731
 dates = [
     d.to_pydatetime().strftime('%Y/%m/3B-DAY.MS.MRG.3IMERG.%Y%m%d')
-    for d in pd.date_range('2000-06-01', '2001-06-01', freq='D')
+    for d in pd.date_range('2000-06-01', '2000-09-01', freq='D')
 ]
 
 
