c7e21aefec8c232e549743038a2c5b07ff8b4f03
Raphael Hagen <norlandrhagen@gmail.com>
2024-03-19 08:42:19 -0600

    local recipe

--- recipes/feedstock/config.py
+++ recipes/feedstock/config.py
@@ -18,3 +18,19 @@ c.InputCacheStorage.fsspec_class = 'fsspec.implementations.local.LocalFileSystem
 c.InputCacheStorage.root_path = 'local_storage/cache/'
 
 
+# c.Bake.prune = 0
+# c.Bake.bakery_class = 'pangeo_forge_runner.bakery.local.LocalDirectBakery'
+# c.LocalDirectBakery.num_workers = 5
+# BUCKET_PREFIX = "s3://pangeo-forge-veda-output"
+# c.TargetStorage.fsspec_class = "s3fs.S3FileSystem"
+# #BUCKET_PREFIX = "/home/jovyan/"
+# #c.TargetStorage.fsspec_class = "fsspec.implementations.local.LocalFileSystem"
+# c.TargetStorage.root_path = f"{BUCKET_PREFIX}/{{job_name}}/output"
+# c.TargetStorage.fsspec_args = {
+#   "anon": False,
+#   "client_kwargs": {"region_name": "us-west-2"}
+# }
+# #c.InputCacheStorage.fsspec_class = c.TargetStorage.fsspec_class
+# #c.InputCacheStorage.fsspec_args = c.TargetStorage.fsspec_args
+# #c.InputCacheStorage.root_path = f"{BUCKET_PREFIX}/cache/"
+
