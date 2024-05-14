a8bc837bb9a1a920bdb4c0be1c6daf07680c991c
Raphael Hagen <norlandrhagen@gmail.com>
2024-05-14 15:21:40 -0600

    oisst test

---
+++ recipes/feedstock/recipe.py
@@ -0,0 +1,55 @@
+import apache_beam as beam
+import pandas as pd
+import s3fs
+from beam_pyspark_runner.pyspark_runner import PySparkRunner
+
+from pangeo_forge_recipes.patterns import ConcatDim, FilePattern
+from pangeo_forge_recipes.storage import FSSpecTarget
+from pangeo_forge_recipes.transforms import (
+    ConsolidateMetadata,
+    OpenURLWithFSSpec,
+    OpenWithXarray,
+    StoreToZarr,
+)
+
+dates = pd.date_range('1981-09-01', '1981-09-03', freq='D')
+
+URL_FORMAT = (
+    'https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation/'
+    'v2.1/access/avhrr/{time:%Y%m}/oisst-avhrr-v02r01.{time:%Y%m%d}.nc'
+)
+
+
+def make_url(time):
+    return URL_FORMAT.format(time=time)
+
+
+time_concat_dim = ConcatDim('time', dates, nitems_per_file=1)
+pattern = FilePattern(make_url, time_concat_dim)
+
+
+# NOTE: source uses the EMR serverless execution role (veda-data-reader-dev)
+source_fsspec_kwargs = {
+    'anon': False,
+    'client_kwargs': {'region_name': 'us-west-2'},
+}
+
+# NOTE: target uses the EMR serverless execution role (veda-data-reader-dev)
+target_fsspec_kwargs = {'anon': False, 'client_kwargs': {'region_name': 'us-west-2'}}
+fs_target = s3fs.S3FileSystem(**target_fsspec_kwargs)
+target_root = FSSpecTarget(fs_target, 's3://veda-pforge-emr-outputs-v4')
+
+
+with beam.Pipeline(runner=PySparkRunner()) as p:
+    (
+        p
+        | beam.Create(pattern.items())
+        | OpenURLWithFSSpec(open_kwargs=source_fsspec_kwargs)
+        | OpenWithXarray(file_type=pattern.file_type)
+        | StoreToZarr(
+            target_root=target_root,
+            store_name='oisst_test.zarr',
+            combine_dims=pattern.combine_dim_keys,
+        )
+        | ConsolidateMetadata()
+    )
