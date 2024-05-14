680194766a1fe811b5d9d22796d1a4f81fcd4481
Raphael Hagen <norlandrhagen@gmail.com>
2024-05-14 12:18:58 -0600

    testing pyspark & ndpyramid w/ pyspark runner

--- recipes/oisst/feedstock/recipe.py
+++ recipes/oisst/feedstock/recipe.py
@@ -1,31 +1,18 @@
-
-
-# pangeo-forge-runner bake \
-# --repo=~/carbonplan/pangeo_forge/staged-recipes/recipes/oisst \
-# -f ~/Documents/carbonplan/pangeo_forge/staged-recipes/recipes/oisst/feedstock \
-# --Bake.recipe_id=oisst \
-# --Bake.job_name=local_test
-
 import apache_beam as beam
-import fsspec
 import pandas as pd
-import zarr
-from apache_beam.runners.interactive.display import pipeline_graph
-from apache_beam.runners.interactive.interactive_runner import InteractiveRunner
+from beam_pyspark_runner.pyspark_runner import PySparkRunner
+
+from pangeo_forge_ndpyramid.transforms import StoreToPyramid
 
 from pangeo_forge_recipes.patterns import ConcatDim, FilePattern
 from pangeo_forge_recipes.storage import FSSpecTarget
-from pangeo_forge_recipes.transforms import (
-    OpenURLWithFSSpec,
-    OpenWithXarray,
-    StoreToPyramid,
-)
+from pangeo_forge_recipes.transforms import OpenURLWithFSSpec, OpenWithXarray
 
-dates = pd.date_range("1981-09-01", "1982-09-01", freq="D")
+dates = pd.date_range('1981-09-01', '1981-09-03', freq='D')
 
 URL_FORMAT = (
-    "https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation/"
-    "v2.1/access/avhrr/{time:%Y%m}/oisst-avhrr-v02r01.{time:%Y%m%d}.nc"
+    'https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation/'
+    'v2.1/access/avhrr/{time:%Y%m}/oisst-avhrr-v02r01.{time:%Y%m%d}.nc'
 )
 
 
@@ -33,22 +20,21 @@ def make_url(time):
     return URL_FORMAT.format(time=time)
 
 
-time_concat_dim = ConcatDim("time", dates, nitems_per_file=1)
+time_concat_dim = ConcatDim('time', dates, nitems_per_file=1)
 pattern = FilePattern(make_url, time_concat_dim)
 
 
 recipe = (
-    beam.Create(pattern.items()) 
-    | OpenURLWithFSSpec() 
+    beam.Create(pattern.items())
+    | OpenURLWithFSSpec()
     | OpenWithXarray(file_type=pattern.file_type)
-    | 'Write Pyramid Levels' >> StoreToPyramid(
-        store_name="pyramid",
-        epsg_code="4326",
-        rename_spatial_dims={"lon": "longitude", "lat": "latitude"},
+    | 'Write Pyramid Levels'
+    >> StoreToPyramid(
+        store_name='pyramid',
+        epsg_code='4326',
+        rename_spatial_dims={'lon': 'longitude', 'lat': 'latitude'},
         n_levels=4,
-        pyramid_kwargs={"extra_dim": "zlev"},
+        pyramid_kwargs={'extra_dim': 'zlev', 'clear_attrs': True},
         combine_dims=pattern.combine_dim_keys,
-    ))
-
-
-
+    )
+)
