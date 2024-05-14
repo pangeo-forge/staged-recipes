7106a6a5c390df0f5ca410715410adc8a1c8f3df
Raphael Hagen <norlandrhagen@gmail.com>
2024-05-14 16:51:21 -0600

    3day

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -14,7 +14,7 @@ from pangeo_forge_recipes.transforms import (
     WriteCombinedReference,
 )
 
-dates = pd.date_range('1981-09-01', '1982-09-01', freq='D')
+dates = pd.date_range('1981-09-01', '1981-09-04', freq='D')
 
 URL_FORMAT = (
     'https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation/'
@@ -44,8 +44,8 @@ with beam.Pipeline(runner=PySparkRunner()) as p:
         | WriteCombinedReference(
             identical_dims=['lat', 'lon', 'zlev'],
             target_root=target_root,
-            store_name='oisst_kerchunk_1_year',
+            store_name='oisst_kerchunk_3_day',
             concat_dims=['time'],
-            output_file_name='combined_oisst_1year.parquet',
+            output_file_name='combined_oisst_3day.parquet',
         )
     )
