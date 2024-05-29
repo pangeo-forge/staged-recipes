c0a12678be86458d1ae7ea7d960c2f2f886976b2
Raphael Hagen <norlandrhagen@gmail.com>
2024-05-29 16:14:38 -0700

    1yr

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -11,7 +11,7 @@ from pangeo_forge_recipes.patterns import ConcatDim, FilePattern
 from pangeo_forge_recipes.storage import FSSpecTarget
 from pangeo_forge_recipes.transforms import OpenURLWithFSSpec, OpenWithXarray
 
-dates = pd.date_range('1981-09-01', '1981-10-01', freq='D')
+dates = pd.date_range('1981-09-01', '1982-09-01', freq='D')
 
 URL_FORMAT = (
     'https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation/'
@@ -54,7 +54,7 @@ with beam.Pipeline(runner=PySparkRunner()) as p:
         | 'Write Pyramid Levels'
         >> StoreToPyramid(
             target_root=target_root,
-            store_name='oisst_pyramid_2_lvl_1_month_pyramid_sync.zarr',
+            store_name='oisst_pyramid_2_lvl_1_year_pyramid_sync.zarr',
             epsg_code='4326',
             rename_spatial_dims={'lon': 'longitude', 'lat': 'latitude'},
             levels=2,
