311a91c909cdad20c02ad0bef7a5970061b33a0a
Raphael Hagen <norlandrhagen@gmail.com>
2024-06-14 17:33:38 -0600

    1yr s3 STZ force sync

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -26,7 +26,7 @@ IDENTICAL_DIMS = ['lat', 'lon']
 
 dates = [
     d.to_pydatetime().strftime('%Y/%m/3B-DAY.MS.MRG.3IMERG.%Y%m%d')
-    for d in pd.date_range('2000-06-01', '2000-12-01', freq='D')
+    for d in pd.date_range('2001-01-01', '2002-01-01', freq='D')
 ]
 URL_FORMAT = (
     'https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation/'
